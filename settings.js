96548b03fedd05a7c8a118c8cd8d23e9804f43a9
Youngboy <nicksonmahachi339@gmail.com>
2025-08-19 19:14:03 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'KADIKI-XMD',
+  author: '',
+  botName: "KADIKI-XMD",
+  botOwner: 'KADIKI-XMD', // Your name
+  ownerNumber: '', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.6",
+};
+
+module.exports = settings;
